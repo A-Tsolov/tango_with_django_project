f7770feecd170eebaefb73c96c0f26e95d6c74e7
Aleksandar Tsolov <2267643@student.gla.ac.uk>
2018-02-07 00:26:18 +0000

    Chapter 8 Complete

--- rango/views.py
+++ rango/views.py
@@ -12,17 +12,17 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
     # return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
 
-
 def about(request):
-    return render(request, 'rango/about.html')
-
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
-    context_dict={}
+    context_dict = {}
 
     try:
         category = Category.objects.get(slug=category_name_slug)
-        pages=Page.objects.filter(category=category)
+        pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
         context_dict['category'] = category
     except Category.DoesNotExist:
